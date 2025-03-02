432c822eefbe4c10c192b86bafd05b676a0665a5
mashtallerdaniil <dmitriip136@gmail.com>
2025-03-02 17:59:19 +0300

    refactoring code

--- src/components/AccountItem.vue
+++ src/components/AccountItem.vue
@@ -1,44 +1,77 @@
 <template>
-  <v-card class="account-item pa-4">
-    <v-text-field
-      v-model="localAccount.rawLabel"
-      label="Метка (необязательно, макс 50 символов)"
-      maxlength="50"
-      :error="errors.label"
-      @blur="onBlur"
-    />
+  <v-card class="mb-4" variant="outlined" max-width="600">
+    <v-card-text>
+      <v-text-field
+        v-model="localAccount.rawLabel"
+        label="Метка"
+        maxlength="50"
+        hint="Необязательное поле, максимум 50 символов"
+        persistent-hint
+        variant="outlined"
+        density="compact"
+        class="mt-4"
+        :error="errors.label"
+        @blur="validate"
+      />
 
-    <v-select
-      v-model="localAccount.type"
-      label="Тип записи"
-      :items="['LDAP', 'Локальная']"
-      @change="onTypeChange"
-    />
+      <v-select
+        v-model="localAccount.type"
+        label="Тип записи"
+        :items="['LDAP', 'Локальная']"
+        variant="outlined"
+        density="compact"
+        class="mt-4"
+        :error="errors.type"
+        @change="onTypeChange"
+      />
 
-    <v-text-field
-      v-model="localAccount.login"
-      label="Логин (обязательно, макс 100 символов)"
-      maxlength="100"
-      :error="errors.login"
-      @blur="onBlur"
-    />
+      <v-text-field
+        v-model="localAccount.login"
+        label="Логин"
+        maxlength="100"
+        hint="Обязательное поле, максимум 100 символов"
+        persistent-hint
+        variant="outlined"
+        density="compact"
+        class="mt-4"
+        required
+        :error="errors.login"
+        @blur="validate"
+      />
 
-    <v-text-field
-      v-if="localAccount.type === 'Локальная'"
-      v-model="localAccount.password"
-      label="Пароль (обязательно, макс 100 символов)"
-      maxlength="100"
-      :error="errors.password"
-      type="password"
-      @blur="onBlur"
-    />
+      <v-text-field
+        v-if="localAccount.type === 'Локальная'"
+        v-model="localAccount.password"
+        label="Пароль"
+        maxlength="100"
+        hint="Обязательное поле, максимум 100 символов"
+        persistent-hint
+        variant="outlined"
+        density="compact"
+        type="password"
+        required
+        :error="errors.password"
+        @blur="validate"
+      />
+    </v-card-text>
 
-    <v-btn color="red" @click="deleteAccount">Удалить</v-btn>
+    <v-card-actions class="px-4 mt-4">
+      <v-spacer />
+      <v-btn
+        color="error"
+        variant="tonal"
+        density="compact"
+        prepend-icon="mdi-delete"
+        @click="deleteAccount"
+      >
+        Удалить
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue';
+import { ref, watchEffect } from 'vue';
 import type { Account } from '../store/useAccountsStore';
 
 const props = defineProps<{ account: Account }>();
@@ -46,46 +79,48 @@ const emit = defineEmits<{ (e: 'update', account: Account): void; (e: 'delete', 
 
 const localAccount = ref({ ...props.account });
 
-const errors = ref({ label: false, login: false, password: false });
+const errors = ref({
+  label: false,
+  type: false,
+  login: false,
+  password: false
+});
 
 function validate() {
-  errors.value = { label: false, login: false, password: false };
+  errors.value.label = localAccount.value.rawLabel?.length > 50;
+  errors.value.type = !localAccount.value.type;
+  errors.value.login = !localAccount.value.login || localAccount.value.login.length > 100;
+  errors.value.password = localAccount.value.type === 'Локальная' && (!localAccount.value.password || localAccount.value.password.length > 100);
 
-  if (localAccount.value.rawLabel.length > 50) errors.value.label = true;
-  if (!localAccount.value.login || localAccount.value.login.length > 100) errors.value.login = true;
-  if (localAccount.value.type === 'Локальная' && (!localAccount.value.password || localAccount.value.password.length > 100)) {
-    errors.value.password = true;
+  if (!Object.values(errors.value).some(Boolean)) {
+    updateAccount();
   }
-
-  return !errors.value.label && !errors.value.login && !errors.value.password;
 }
 
 function updateAccount() {
-  if (validate()) {
-    localAccount.value.label = localAccount.value.rawLabel.split(';').map(text => ({ text: text.trim() }));
-    if (localAccount.value.type === 'LDAP') localAccount.value.password = null;
-    emit('update', { ...localAccount.value });
+  localAccount.value.label = localAccount.value.rawLabel
+    ?.split(';')
+    .map(text => ({ text: text.trim() })) ?? [];
+    
+  if (localAccount.value.type === 'LDAP') {
+    localAccount.value.password = null;
   }
-}
 
-function onBlur() {
-  updateAccount();
+  emit('update', { ...localAccount.value });
 }
 
 function onTypeChange() {
-  if (localAccount.value.type === 'LDAP') localAccount.value.password = null;
-  updateAccount();
+  if (localAccount.value.type === 'LDAP') {
+    localAccount.value.password = null;
+  }
+  validate();
 }
 
 function deleteAccount() {
   emit('delete', localAccount.value.id);
 }
 
-watch(() => props.account, newVal => { localAccount.value = { ...newVal }; });
+watchEffect(() => {
+  localAccount.value = { ...props.account };
+});
 </script>
-
-<style scoped>
-.account-item {
-  margin-bottom: 1rem;
-}
-</style>
--- src/components/AccountsManager.vue
+++ src/components/AccountsManager.vue
@@ -1,15 +1,33 @@
 <template>
-  <v-container>
-    <v-card class="pa-4 d-flex">
-      <v-card-title class="text-h5">Управление учетными записями</v-card-title>
-      <v-card-actions>
-        <v-btn class="white" @click="addAccount">Добавить учетную запись</v-btn>
-      </v-card-actions>
+  <v-container class="mt-4">
+    <v-card class="mb-6" flat>
+      <v-card-item class="pa-4 bg-primary">
+        <div class="d-flex align-center justify-space-between">
+          <v-card-title class="text-h5 text-white">Управление учетными записями</v-card-title>
+          <v-btn
+            color="white"
+            variant="elevated"
+            prepend-icon="mdi-plus"
+            @click="addAccount"
+          >
+            Добавить учетную запись
+          </v-btn>
+        </div>
+      </v-card-item>
     </v-card>
 
-    <v-row>
-      <v-col v-for="account in accountsStore.accounts" :key="account.id" cols="12">
-        <AccountItem :account="account" @update="updateAccount" @delete="deleteAccount" />
+    <v-row class="mx-0" density="compact">
+      <v-col
+        v-for="account in accountsStore.accounts"
+        :key="account.id"
+        rows="2" cols="3" md="8" lg="6"
+        class="py-2"
+      >
+        <AccountItem
+          :account="account"
+          @update="updateAccount"
+          @delete="deleteAccount"
+        />
       </v-col>
     </v-row>
   </v-container>
