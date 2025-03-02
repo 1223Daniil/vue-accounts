7e0bbcf887f4b62330bba2371d0681bffc2b13e5
mashtallerdaniil <dmitriip136@gmail.com>
2025-03-02 21:17:58 +0300

    fix vuetify

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import './style.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
 
 const vuetify = createVuetify({
     components,
